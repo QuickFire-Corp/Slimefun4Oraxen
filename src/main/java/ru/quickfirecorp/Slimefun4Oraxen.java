3a945d284ec6d577450eafa94afe9c81abd9cc5e
Dancbeunny98 <jackworldppooii@gmail.com>
2024-01-19 20:23:41 +0300

    .

--- src/main/java/ru/quickfirecorp/Slimefun4Oraxen.java
+++ src/main/java/ru/quickfirecorp/Slimefun4Oraxen.java
@@ -1,17 +1,30 @@
 package ru.quickfirecorp;
 
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Slimefun4Oraxen extends JavaPlugin {
+public class Slimefun4Oraxen extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Config cfg = new Config(this);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Logic for disabling the plugin...
+    }
+
+    @Override
+    public String getBugTrackerURL() {
+        // You can return a link to your Bug Tracker instead of null here
+        return null;
+    }
+
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return this;
     }
 }
